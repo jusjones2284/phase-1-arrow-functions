b36ae3e62839acce7e27b0621e89b541567a2188
Justin Jones <jus.jones@yahoo.com>
2022-03-22 21:13:07 -0400

    commit tree 0d798a52c31f086715e1441fb04b6d2b01fa7dc2

---
+++ index.js
@@ -0,0 +1,54 @@
+function foo() {
+    return 'bar';
+}
+
+function foos(){
+    return 'bars'
+}
+const fooes = function (){
+    return 'bar';
+}
+
+const fooss = function (){
+    return 'bar';
+}
+
+const sum = 1 + 1
+
+const difference = 10 - 1
+
+const add = (parameter1, parameter2) => parameter1 + parameter2
+
+add(5,5);
+
+const adds = (parm1, parm2) => parm1 * parm2;
+
+adds(8,15);
+
+const added = (parms1, parm22) => parms1 * parm22;
+
+added(8, 24)
+
+const twoAdder = x => x + 2;
+
+const twoAdders = (y) => y / 4;
+
+twoAdders(8);
+
+const threeAdders = (z, p = 2) => z ** 4/p
+
+threeAdders(10)
+
+const nums = [1,2,3];
+
+const squares = nums.map(x => x ** 2);
+squares;
+nums;
+
+
+function divide(){
+    return 2000 / 100
+
+}
+
+const square = (yahoo) => yahoo * yahoo
